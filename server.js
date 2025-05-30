095c552c693ae3476a63dd19e6e273e8143a7025
Varshitha17-git <varshitha092003@gmail.com>
2025-05-31 00:06:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.post('/api/exams', (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
-    console.log(`Server running at http://:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
